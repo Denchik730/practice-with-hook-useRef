6059f8e6f52c60e506d05076e1e586f156ca5965
Denis <gabbas.denn@mail.ru>
2022-09-09 06:01:01 +0300

    practice wint hook useRef

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useEffect } from 'react';
+import {useRef, useState} from 'react';
+import {Container} from 'react-bootstrap';
+// import { ThemeConsumer } from 'react-bootstrap/esm/ThemeProvider';
+import './App.css';
+
+const Form = () => {
+    const [text, setText] = useState('');
+
+    const myRef = useRef(1);
+
+    useEffect(() => {
+        console.log(myRef.current);
+    })
+
+
+        return (
+            <Container>
+                <form className="w-50 border mt-5 p-3 m-auto">
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+						<input onChange={(e) => setText(e.target.value)} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea onClick={() => myRef.current + 1} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                </form>
+            </Container>
+        )
+}
+
+// class TextInput extends Component {
+
+// 	doSmth = () => {
+// 		console.log('smth');
+// 	}
+	
+// 	render() {
+// 		return (
+// 			<input  type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+// 		)
+// 	}
+// }
+
+function App() {
+    return (
+        <Form/>
+    );
+}
+
+export default App;
